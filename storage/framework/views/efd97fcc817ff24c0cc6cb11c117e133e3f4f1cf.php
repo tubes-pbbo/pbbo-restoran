e6810bc89a52acd907fec280006b62e308b67863
beatfraps <57259440+beatfraps@users.noreply.github.com>
2020-11-12 10:14:28 +0700

    delete

--- storage/framework/views/efd97fcc817ff24c0cc6cb11c117e133e3f4f1cf.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div class="footer1">
-      <p style="margin-bottom: -5px">Restaurant OKE</p>
-      <p style="margin-top: -5px">Jalan Apa Aja Yang Penting Oke</p>
-    </div>
-</footer><?php /**PATH C:\Users\FeraniSyantik\pbbo\resources\views/footer.blade.php ENDPATH**/ ?>
